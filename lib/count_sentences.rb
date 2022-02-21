e4200e511ddce10cf12f15d07eee3cf4cfae1627
Louise Iyengar <louise.iyengar@flatironschool.com>
2022-02-20 23:11:21 -0500

    Done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[self.length - 1] == "."
   end
 
   def question?
-
+    self[self.length - 1] == "?"
   end
 
   def exclamation?
-
+    self[self.length - 1] == "!"
   end
 
   def count_sentences
-
+    test = self.split(/\.|\?|!/).filter{|sentence| sentence !=''}.length
   end
 end
