def2d285e7651189b78d33db9772990e9a883fa9
Louise Iyengar <louise.iyengar@flatironschool.com>
2022-02-20 23:10:53 -0500

    commit tree 410f7c078b871e3b43771c264135731a5fe950a0

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[self.length - 1] == "."
   end
 
   def question?
-
+    self[self.length - 1] == "?"
   end
 
   def exclamation?
-
+    self[self.length - 1] == "!"
   end
 
   def count_sentences
-
+    test = self.split(/\.|\?|!/).filter{|sentence| sentence !=''}.length
+    p test
   end
+
+  "This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
 end
